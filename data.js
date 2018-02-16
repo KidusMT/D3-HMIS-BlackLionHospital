b1432feccd30e7bb1d67a012b71a64993ce524fa
Samuel Mussie <samuel.mussie@yandex.com>
2018-02-16 17:49:33 +0300

    moved the data for the tree into a new file

---
+++ data.js
@@ -0,0 +1,334 @@
+
+
+data = {
+    "name": "AAU Board",
+    "full_name": "Addis Ababa University Board",
+    "parent": "null",
+    "description": [
+        {
+            "info": "",
+            "contact_name": "",
+            "phone": ""
+        }
+    ],
+    "children": [
+        {
+            "name": "President",
+            "full_name": "AAU Office of the President",
+            "parent": "AAU Board",
+            "description": [
+                {
+                    "info": "",
+                    "contact_name": "",
+                    "phone": ""
+                }
+            ],
+            "children": [
+                {
+                    "name": "TASH Board",
+                    "full_name": "TASH Governing Board",
+                    "parent": "President",
+                    "description": [
+                        {
+                            "info": "",
+                            "contact_name": "",
+                            "phone": ""
+                        }
+                    ],
+                    "children": [
+                        {
+                            "name": "OED for CHS",
+                            "full_name": "OED for CHS with BP rank",
+                            "parent": "TASH Board",
+                            "description": [
+                                {
+                                    "info": "",
+                                    "contact_name": "",
+                                    "phone": ""
+                                }
+                            ],
+                            "children": [
+                                {
+                                    "name": "Dir research",
+                                    "full_name": "Director for Research",
+                                    "parent": "OED for CHS",
+                                    "description": [
+                                        {
+                                            "info": "",
+                                            "contact_name": "",
+                                            "phone": ""
+                                        }
+                                    ],
+                                    "children": []
+                                },
+                                {
+                                    "name": "Dir. Clinical",
+                                    "full_name": "Director for Clinical Services",
+                                    "parent": "OED for CHS",
+                                    "description": [
+                                        {
+                                            "info": "",
+                                            "contact_name": "",
+                                            "phone": ""
+                                        }
+                                    ],
+                                    "_children": [
+                                        {
+                                            "name": "Nursing",
+                                            "full_name": "Nursing Directorate",
+                                            "parent": "Dir. Clinical",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "Sister Sara",
+                                                    "phone": "0913017215"
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": "Clinical Dep.",
+                                            "full_name": "Clinical Departments",
+                                            "parent": "Dir. Clinical",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "",
+                                                    "phone": ""
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": "Pharmacy",
+                                            "full_name": "Pharmacy and Medical Supplies Directorate",
+                                            "parent": "Dir. Clinical",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "Mr. Bizuayehu",
+                                                    "phone": "0911803341"
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": "Diagnostics",
+                                            "full_name": "Diagnostics Directorate",
+                                            "parent": "Dir. Clinical",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "",
+                                                    "phone": ""
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": "OPD",
+                                            "full_name": "Outpatient Directorate",
+                                            "parent": "Dir. Clinical",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "",
+                                                    "phone": ""
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": "OR",
+                                            "full_name": "Operation Theatre Directorate",
+                                            "parent": "Dir. Clinical",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "Mr. Yesuf",
+                                                    "phone": " 0913109671"
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": "Biomedical",
+                                            "full_name": "Biomedical Directorate",
+                                            "parent": "Dir. Clinical",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "",
+                                                    "phone": ""
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": "ER",
+                                            "full_name": "Emergency Directorate",
+                                            "parent": "Dir. Clinical",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "",
+                                                    "phone": ""
+                                                }
+                                            ],
+                                            "children": []
+                                        }
+                                    ]
+                                },
+                                {
+                                    "name": "Dir. Academic",
+                                    "full_name": "Director for Academic Affairs",
+                                    "parent": "OED for CHS",
+                                    "description": [
+                                        {
+                                            "info": "",
+                                            "contact_name": "",
+                                            "phone": ""
+                                        }
+                                    ],
+                                    "_children": [
+                                        {
+                                            "name": "Associate D.",
+                                            "full_name": "Assiciate Director for Academic Standards and Staff Affairs",
+                                            "parent": "Dir. Academic",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "",
+                                                    "phone": ""
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": "All school D.",
+                                            "full_name": "All School Deans",
+                                            "parent": "Dir. Academic",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "",
+                                                    "phone": ""
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": "Non-Clinical",
+                                            "full_name": "Non-Clinical Departments",
+                                            "parent": "Dir. Academic",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "",
+                                                    "phone": ""
+                                                }
+                                            ],
+                                            "children": []
+                                        }
+                                    ]
+                                },
+                                {
+                                    "name": "Dir. Business",
+                                    "full_name": "Director for Business Development and Adminsitration",
+                                    "parent": "OED for CHS",
+                                    "description": [
+                                        {
+                                            "info": "",
+                                            "contact_name": "",
+                                            "phone": ""
+                                        }
+                                    ],
+                                    "_children": [
+                                        {
+                                            "name": "Budget & finance",
+                                            "full_name": "Budget and Finances",
+                                            "parent": "Dir. Business",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "",
+                                                    "phone": ""
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": "General service",
+                                            "full_name": "General Services",
+                                            "parent": "Dir. Business",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "",
+                                                    "phone": ""
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": "Property & resource",
+                                            "full_name": "Property Adminstration and Resource Management",
+                                            "parent": "Dir. Business",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "Selam",
+                                                    "phone": "0910356992"
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": "HRM & Dev't",
+                                            "full_name": "HRM and Development",
+                                            "parent": "Dir. Business",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "Mr. Tamrat Siyum ",
+                                                    "phone": "0911998348 "
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": " Procurement",
+                                            "full_name": "Department of Procurement",
+                                            "parent": "Dir. Business",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "Ms Emawash",
+                                                    "phone": "0911839630"
+                                                }
+                                            ],
+                                            "children": []
+                                        },
+                                        {
+                                            "name": "Student affair",
+                                            "full_name": "Student Affairs",
+                                            "parent": "Dir. Business",
+                                            "description": [
+                                                {
+                                                    "info": "",
+                                                    "contact_name": "Ato Tesfaye",
+                                                    "phone": "0932250662"
+                                                }
+                                            ],
+                                            "children": []
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
